fbd6b9d607c1b8de214894de1731e4d0a41d5461
JaviPedrote <pedrote3.1416@gmail.com>
2025-03-11 14:55:09 +0100

    boton eliminar

--- src/components/OrderContens.tsx
+++ src/components/OrderContens.tsx
@@ -10,14 +10,14 @@ export const OrderContens = ({ order , deleteItem }: orderProps) => {
   return (
     <>
       <div className="text-4xl font-black">Ticket</div>
-      <div className="space-y-2 mt-5">
+      <div className=" mt-5 ">
         {order.length === 0 ? (
           <p className="text-center">No hay productos en el ticket</p>
         ) : (
           order.map((item) => (
             <div
               key={item.id}
-              className="flex justify-between border-t-2 border-gray-200 p-2 last-of-type:border-b-2"
+              className="flex py-3 justify-between border-t-2 border-gray-200 p-2 last-of-type:border-b-2"
             >
               <div>
                 <p className="lg:text-lg">
@@ -30,9 +30,9 @@ export const OrderContens = ({ order , deleteItem }: orderProps) => {
               <div className="flex items-center">
                 <button
                   onClick={() => deleteItem(item.id)}
-                  className="flex justify-center items-center bg-red-400 h-6 w-6 rounded-full"
+                  className="flex justify-center items-center text-red-600 w-6 h-6 font-black cursor-pointer"
                 >
-                  <p className="text-xs font-semibold text-white">X</p>
+                    X
                 </button>
               </div>
             </div>
